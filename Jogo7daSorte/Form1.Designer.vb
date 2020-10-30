c8991f1a01d199539f71d6703e34446eb3da0b08
devleonardo <leonardomartins.ads@gmail.com>
2020-10-30 00:05:25 -0300

    first commit

---
+++ Jogo7daSorte/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.num1 = New System.Windows.Forms.Label()
+        Me.num2 = New System.Windows.Forms.Label()
+        Me.num3 = New System.Windows.Forms.Label()
+        Me.imagemResultado = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.imagemResultado, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Font = New System.Drawing.Font("Wild Hazelnut", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(167, -6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(432, 83)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Jogo 7 da Sorte"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.num3)
+        Me.Panel1.ForeColor = System.Drawing.Color.Red
+        Me.Panel1.Location = New System.Drawing.Point(427, 147)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(62, 70)
+        Me.Panel1.TabIndex = 1
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.White
+        Me.Panel2.Controls.Add(Me.num2)
+        Me.Panel2.Location = New System.Drawing.Point(337, 147)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(63, 70)
+        Me.Panel2.TabIndex = 0
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.White
+        Me.Panel3.Controls.Add(Me.num1)
+        Me.Panel3.Location = New System.Drawing.Point(248, 147)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(61, 70)
+        Me.Panel3.TabIndex = 0
+        '
+        'num1
+        '
+        Me.num1.AutoSize = True
+        Me.num1.Font = New System.Drawing.Font("Miss Scarlett Plain", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num1.ForeColor = System.Drawing.Color.Red
+        Me.num1.Location = New System.Drawing.Point(1, 3)
+        Me.num1.Name = "num1"
+        Me.num1.Size = New System.Drawing.Size(68, 65)
+        Me.num1.TabIndex = 0
+        Me.num1.Text = "0"
+        '
+        'num2
+        '
+        Me.num2.AutoSize = True
+        Me.num2.Font = New System.Drawing.Font("Miss Scarlett Plain", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num2.ForeColor = System.Drawing.Color.Red
+        Me.num2.Location = New System.Drawing.Point(1, 3)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(68, 65)
+        Me.num2.TabIndex = 0
+        Me.num2.Text = "0"
+        '
+        'num3
+        '
+        Me.num3.AutoSize = True
+        Me.num3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.num3.Font = New System.Drawing.Font("Miss Scarlett Plain", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num3.ForeColor = System.Drawing.Color.Red
+        Me.num3.Location = New System.Drawing.Point(1, 3)
+        Me.num3.Name = "num3"
+        Me.num3.Size = New System.Drawing.Size(68, 65)
+        Me.num3.TabIndex = 0
+        Me.num3.Text = "0"
+        '
+        'imagemResultado
+        '
+        Me.imagemResultado.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.imagemResultado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.imagemResultado.Location = New System.Drawing.Point(226, 223)
+        Me.imagemResultado.Name = "imagemResultado"
+        Me.imagemResultado.Size = New System.Drawing.Size(285, 176)
+        Me.imagemResultado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.imagemResultado.TabIndex = 2
+        Me.imagemResultado.TabStop = False
+        Me.imagemResultado.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Teal
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Montserrat", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(609, 147)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 70)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Jogar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Red
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Montserrat", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(609, 363)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(113, 36)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Sair"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(734, 411)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.imagemResultado)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Jogo 7 da Sorte"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.imagemResultado, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents num3 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents num2 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents num1 As Label
+    Friend WithEvents imagemResultado As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Jogo7daSorte/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        imagemResultado.Visible = False
+        num1.Text = CStr(Int(Rnd() * 10)) 'Escolhe 1º número aleatório de 1 a 10
+        num2.Text = CStr(Int(Rnd() * 10)) 'Escolhe 2º número aleatório de 1 a 10
+        num3.Text = CStr(Int(Rnd() * 10)) 'Escolhe 3º número aleatório de 1 a 10
+        'Se todos os numeros forem iguais a 7
+        If (num1.Text = "7") And (num2.Text = "7") And (num3.Text = "7") Then
+            imagemResultado.Image = Image.FromFile("C:\Users\leona\source\repos\Jogo7daSorte\GanhouMax.jpg")
+            imagemResultado.Visible = True
+            Beep()
+            'Se um dos numeros forem iguais a 7
+        ElseIf (num1.Text = "7") Or (num2.Text = "7") Or (num3.Text = "7") Then
+            imagemResultado.Image = Image.FromFile("C:\Users\leona\source\repos\Jogo7daSorte\Ganhou.jpg")
+            imagemResultado.Visible = True
+            Beep()
+            'Se nenhum numero for igual a 7
+        Else
+            imagemResultado.Image = Image.FromFile("C:\Users\leona\source\repos\Jogo7daSorte\Perdeu.jpg")
+            imagemResultado.Visible = True
+            Beep()
+        End If
+
+    End Sub
+End Class
